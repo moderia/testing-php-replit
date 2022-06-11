f178e237d5506a293b12048fba6e502ff07f8f40
Mohamed Deria <103150230+moderia@users.noreply.github.com>
2022-06-11 20:52:51 +0000

    adds subtract test

---
+++ Calculator-subtract-test.php
@@ -0,0 +1,13 @@
+<?php
+//calculator-subtract-est.php
+require_once 'simpletest/autorun.php';
+include 'Calculator.php';
+
+class TestOfCalculator extends UnitTestCase {
+	function testSubtract() {
+	  $myCalc = new Calculator();
+	  $this->assertEqual($myCalc->subtract(4,2), 2, "4 - 2");
+	  $this->assertEqual($myCalc->subtract(7,3), 4, "7 - 3");
+	}
+	
+}
